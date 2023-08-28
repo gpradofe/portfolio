131d356b43e7fa3bbcbd69651685a39a64849b33
gpradofe <gpradofe@nd.edu>
2023-08-28 15:15:35 -0400

    Commit - Adjusting basic laptop

--- porfolio/src/Pages/LandingPage/index.tsx
+++ porfolio/src/Pages/LandingPage/index.tsx
@@ -1,50 +1,50 @@
-import React from "react";
-import {
-  ShaderMaterial,
-  Mesh,
-  TextureLoader,
-  MeshStandardMaterial,
-} from "three";
-import { Canvas } from "@react-three/fiber";
+import React, { useEffect, useRef } from "react";
+import { AnimationMixer, LoopRepeat, PerspectiveCamera } from "three";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
 import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
-import notebookModel from "../../Assets/3DModels/notebook.gltf";
-import notebookTexture from "../../Assets/Textures/keyboard-texture.png";
+import notebookModel from "../../Assets/3DModels/betterNotebook.glb";
 
-const gradientTexture = new TextureLoader().load(notebookTexture);
-const gradientMaterial = new MeshStandardMaterial({ map: gradientTexture });
+function CameraSetter() {
+  const { camera } = useThree();
 
+  useEffect(() => {
+    const perspCamera = camera as PerspectiveCamera;
+    perspCamera.fov = 55; // Set this to your desired FOV
+    perspCamera.updateProjectionMatrix();
+  }, [camera]);
+
+  return null;
+}
 function Notebook() {
   const gltf = useGLTF(notebookModel) as GLTF;
 
-  gltf.scene.traverse((child: THREE.Object3D) => {
-    if (child.name === "macBook_BottomPart" && "isGroup" in child) {
-      (child as THREE.Group).children.forEach((subChild: THREE.Object3D) => {
-        if ("isMesh" in subChild) {
-          if (
-            subChild.name === "Cube005_5" ||
-            subChild.name === "Cube005_4" ||
-            subChild.name === "Cube005_3" ||
-            subChild.name === "Cube005_2"
-          ) {
-            (subChild as THREE.Mesh).material = gradientMaterial;
-          }
-        }
-      });
+  const mixer = useRef<AnimationMixer>();
+
+  useEffect(() => {
+    if (gltf.animations.length > 0) {
+      mixer.current = new AnimationMixer(gltf.scene);
+      const action = mixer.current.clipAction(gltf.animations[0]);
+      action.play();
+    }
+  }, [gltf]);
+
+  useFrame((state, delta) => {
+    if (mixer.current) {
+      mixer.current.update(delta);
     }
   });
-  gltf.scene.position.set(-0.7, -2, -0.4);
-  return (
-    <primitive object={gltf.scene} rotation={[0, Math.PI, 0]} dispose={null} />
-  );
+  gltf.scene.position.set(0, -0.1, 4.65);
+  return <primitive object={gltf.scene} dispose={null} />;
 }
 
 const LandingPage: React.FC = () => {
   return (
     <Canvas style={{ height: "100vh" }}>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 10, 5]} intensity={1} />
+      <ambientLight intensity={5} />
+      <pointLight position={[0, 0.6, 4]} intensity={4} />
       <Notebook />
+      <CameraSetter />
     </Canvas>
   );
 };
